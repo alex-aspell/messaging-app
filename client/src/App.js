25fb1b68f524b47af644af522ff8eab2f5f7133e
Alex Aspell <alexgaspell@gmail.com>
2020-02-20 16:49:15 -0600

    Generates template from message

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import ReactMustache from 'react-mustache';
 import './App.css';
 
 function App() {
@@ -10,81 +10,89 @@ function App() {
     guests: [],
     message: '',
     company: '',
-    guest: ''
+    guest: '',
+    showTemplate: false,
+    showCustom: false
   });
 
-  console.log('data', data);
+  const [template, setTemplate] = useState({welcome: '', template: "Select message template"});
 
+  console.log('data', data);
+  
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('/get/messages');
       const body = await response.json();
       if (response.status !== 200) throw Error(body.message);
-      console.log('body', body);
-      // return body;
       
-      setData({messages: body.messages, companies: body.companies, guests: body.guests});
+      setData({...data, messages: body.messages, companies: body.companies, guests: body.guests});
+    }
+
+    function createWelcome() {
+      var today = new Date()
+      var curHr = today.getHours()
+  
+      if (curHr < 12) {
+        setTemplate({...template, welcome: "Good morning"});
+      } else if (curHr < 18) {
+        setTemplate({...template, welcome: "Good afternoon"});
+      } else {
+        setTemplate({...template, welcome: "Good evening"});
+      }
     }
     fetchData();
+    createWelcome();
   }, []);
 
-  async function generateMessage() {
-    const payload = {
-      message: data.message,
-      company: data.company,
-      guest: data.guest
-    }
+  function selectOption(id, option) {
+    let selected = data[option].find(item => item.id == id);
+    setTemplate({...template, ...selected});
+  } 
 
-    console.log('payload', payload);
-    const response = await fetch('/post', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-    const body = await response.text();
-  }
   
+
+  console.log('template', template);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Message App</h2>
       </header>
       <div>
-        <select onChange={e => setData({...data, message: e.target.value})}>
+        <select onChange={e => selectOption(e.target.value, "messages")}>
           <option>Select a message</option>
           {data.messages.map(item => (
             <option value={item.id}>{item.description}</option>
           ))}
         </select>
-        <select onChange={e => setData({...data, company: e.target.value})}>
+        <select onChange={e => selectOption(e.target.value, "companies")}>
           <option>Select a company</option>
           {data.companies.map(item => (
             <option value={item.id}>{item.company}</option>
           ))}
         </select>
-        <select onChange={e => setData({...data, guest: e.target.value})}>
+        <select onChange={e => selectOption(e.target.value, "guests")}>
           <option>Select a guest</option>
           {data.guests.map(item => (
             <option value={item.id}>{item.firstName} {item.lastName}</option>
           ))}
         </select>
-        <button onClick={generateMessage}>
+        <button onClick={() => setData({...data, showCustom: true})}>
+          Custom
+        </button>
+        <button onClick={() => setData({...data, showTemplate: true})}>
           Generate Message
         </button>
+        {data.showCustom && 
+          <div>
+            <textarea onChange={e => setTemplate({...template, template: e.target.value})}/>
+          </div>
+        }
+        {data.showTemplate &&
+          <div>
+            <ReactMustache template={template.template} data={template} />
+          </div>
+        }
       </div>
     </div>
   );
